83a7a035fb1ef87ff9e01d1af77e48223b80d75d
José Froes <josepaulo.froes@gmail.com>
2025-11-13 16:05:10 -0300

    Exercicio 03: Crie Carro com atributos marca, modelo, ano.

---
+++ src/Carro.java
@@ -0,0 +1,30 @@
+//Exercicio 03: Crie Carro com atributos marca, modelo, ano.
+public class Carro {
+    private String marca;
+    private String modelo;
+    private Integer ano;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
