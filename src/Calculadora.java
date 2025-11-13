37d7a6387520192ce1d371d154a78ed74dd8cbae
José Froes <josepaulo.froes@gmail.com>
2025-11-13 17:53:31 -0300

    Exercicio 10: Crie Calculadora com métodos estáticos somar, dividir, etc.

---
+++ src/Calculadora.java
@@ -0,0 +1,20 @@
+public class Calculadora {
+// Exercicio 10: Crie Calculadora com métodos estáticos somar, dividir, etc.
+
+    public static Integer somar(Integer a, Integer b){
+        return a + b;
+    }
+
+    public static Integer subtrair(Integer a, Integer b){
+        return a - b;
+    }
+
+    public static Integer multiplicar(Integer a, Integer b){
+        return a * b;
+    }
+
+    public static Integer dividir(Integer a, Integer b){
+        return a / b;
+    }
+}
+
